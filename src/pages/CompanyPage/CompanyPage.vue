7750cf605b753205318ff8d553d7eb326e869b44
Mark James <142849183+markjamesgit@users.noreply.github.com>
2025-06-03 21:45:53 +0800

    feat: first commit

---
+++ src/pages/CompanyPage/CompanyPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-page>
+    <div class="q-pa-md text-h4 text-center">
+      Welcome to Cafe Flor Company Page
+    </div>
+  </q-page>
+</template>
+
+<script lang="ts">
+export default {
+  data() { return {}; },
+  mounted() {},
+  methods: {},
+  computed: {}
+};
+</script> 
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,18 +1,12 @@
 import type { RouteRecordRaw } from 'vue-router';
 
 const routes: RouteRecordRaw[] = [
-  {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
-  },
-
-  // Always leave this as last one,
-  // but you can also remove it
-  {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue'),
-  },
+  { path: '/', component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/LandingPage/LandingPage.vue') },
+      { path: 'company', component: () => import('pages/CompanyPage/CompanyPage.vue') }
+    ] },
+  { path: '/:catchAll(.*)*', component: () => import('pages/ErrorNotFound.vue') }
 ];
 
 export default routes;
