87b263e2839a97abebd11a456687f4a6af1fc889
Mark James <142849183+markjamesgit@users.noreply.github.com>
2025-06-04 18:51:55 +0800

    feat: barista premium page

--- src/pages/LandingPage/BaristaPremiumPage/BaristaPremiumPage.vue
+++ src/pages/LandingPage/BaristaPremiumPage/BaristaPremiumPage.vue
@@ -9,6 +9,80 @@
       </p>
     </div>
   </section>
+
+  <div class="our-passion-container">
+    <div class="background-images">
+      <img
+        src="../../../assets/icons/splash4.png"
+        alt="Background decoration 1"
+        class="bg-image bg-image-1"
+      />
+      <img
+        src="../../../assets/icons/milktea.png"
+        alt="Background decoration 2"
+        class="bg-image bg-image-2"
+      />
+      <img
+        src="../../../assets/icons/mojito.png"
+        alt="Background decoration 3"
+        class="bg-image bg-image-3"
+      />
+      <img
+        src="../../../assets/icons/steak.png"
+        alt="Background decoration 4"
+        class="bg-image bg-image-4"
+      />
+      <img
+        src="../../../assets/icons/splash5.png"
+        alt="Background decoration 5"
+        class="bg-image bg-image-5"
+      />
+    </div>
+    <div class="barista-premium">
+      <div class="first-row">
+        <img src="../../../assets/background-images/exp1.png" alt="Barista Premium" />
+        <p>
+          Where great vibes and even greater brews come together. Let’s make your day delicious.
+        </p>
+      </div>
+      <div class="second-row">
+        <img
+          class="exp1-img"
+          src="../../../assets/background-images/exp2.png"
+          alt="Barista Premium"
+        />
+        <div class="text-container">
+          <p class="left-text">DELIGHTFULL</p>
+          <p class="right-text">EXPERIENCE</p>
+        </div>
+        <img
+          class="exp2-img"
+          src="../../../assets/background-images/exp3.png"
+          alt="Barista Premium"
+        />
+      </div>
+      <div class="third-row">
+        <p>
+          Indulge in elevated taste and timeless ambiance. This is more than coffee and bar, it’s a
+          premium experience, made just for you.
+        </p>
+      </div>
+    </div>
+  </div>
+
+  <section class="billiard-section">
+    <div class="billiard-content">
+      <h1 class="billiard-title">
+        <span>The<br /></span>BAR
+      </h1>
+      <p class="billiard-subtitle">
+        Kick back, cue up, and let the night roll at THE BAR. With cold drinks in hand, good music
+        in the air, and billiards ready for the taking, it’s the perfect spot to unwind and vibe
+        with friends. Whether you're starting the night or staying 'til last call, THE BAR brings
+        the right mix of fun, flavor, and feel-good energy.
+      </p>
+    </div>
+  </section>
 </template>
 
 <script lang="ts">
