2f88beeb6a7c00290f16ca1eff6f921771aa3bf8
Mark James <142849183+markjamesgit@users.noreply.github.com>
2025-06-04 17:11:36 +0800

    feat: barista premium page

---
+++ src/pages/LandingPage/BaristaPremiumPage/BaristaPremiumPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="hero-section">
+    <div class="hero-content">
+      <img src="../../../assets/logo/bp-logo.png" alt="Barista Premium Logo" />
+      <h1 class="hero-title">BARISTA PREMIUM</h1>
+      <p class="hero-subtitle">
+        Blending innovation with bold flavor, we bring premium quality to every cup, empowering
+        partners to accelerate the market with confidence and distinction.
+      </p>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'BaristaPremiumPage',
+};
+</script>
+<style lang="scss" scoped>
+@import './BaristaPremiumPage.scss';
+</style>
