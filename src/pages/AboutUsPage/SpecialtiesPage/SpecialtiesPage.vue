552ad6ec3a1a33fa1fd2a382c5ffb5f95e507c40
Mark James <142849183+markjamesgit@users.noreply.github.com>
2025-06-05 23:55:00 +0800

    feat: added sppecial page

--- src/pages/AboutUsPage/SpecialtiesPage/SpecialtiesPage.vue
+++ src/pages/AboutUsPage/SpecialtiesPage/SpecialtiesPage.vue
@@ -1,6 +1,25 @@
 <template>
-  <div>
-    <p>SpecialtiesPage</p>
+  <div class="our-passion-container">
+    <div class="background-images">
+      <img
+        src="../../../assets/icons/steak.png"
+        alt="Background decoration 2"
+        class="bg-image bg-image-2"
+      />
+    </div>
+
+    <div class="container">
+      <div class="first-section">
+        <p>OUR SPECIALTIES</p>
+        <div class="horizontal-line"></div>
+      </div>
+      <div class="second-section">
+        <img src="../../../assets/background-images/special1.png" alt="" />
+        <img src="../../../assets/background-images/special2.png" alt="" />
+        <img src="../../../assets/background-images/special3.png" alt="" />
+        <img src="../../../assets/background-images/special4.png" alt="" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,3 +28,6 @@ export default {
   name: 'SpecialtiesPage',
 };
 </script>
+<style lang="scss" scoped>
+@import './SpecialtiesPage.scss';
+</style>
