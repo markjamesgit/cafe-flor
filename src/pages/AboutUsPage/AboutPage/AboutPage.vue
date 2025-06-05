f0dd0316ab9b0d98a3b270a9a6cbd568e186e333
Mark James <142849183+markjamesgit@users.noreply.github.com>
2025-06-05 23:30:24 +0800

    feat: about-page added

--- src/pages/AboutUsPage/AboutPage/AboutPage.vue
+++ src/pages/AboutUsPage/AboutPage/AboutPage.vue
@@ -1,6 +1,77 @@
 <template>
-  <div>
-    <p>AboutPage</p>
+  <div class="our-passion-container">
+    <div class="background-images">
+      <img
+        src="../../../assets/icons/pastry.png"
+        alt="Background decoration 1"
+        class="bg-image bg-image-1"
+      />
+      <img
+        src="../../../assets/icons/splash6.png"
+        alt="Background decoration 2"
+        class="bg-image bg-image-2"
+      />
+
+      <img
+        src="../../../assets/icons/splash3.png"
+        alt="Background decoration 3"
+        class="bg-image bg-image-3"
+      />
+    </div>
+
+    <div class="first-row">
+      <div class="left-section">
+        <img src="../../../assets/background-images/about1.png" alt="About Us" />
+        <p>ABOUT US</p>
+      </div>
+      <div class="right-section">
+        <div class="top-section">
+          <p>Where every sip blooms.</p>
+          <p class="description">
+            Welcome to Cafe Flor + Bar where mornings bloom and nights come alive. Start your day
+            with the warmth of handcrafted coffee, fresh pastries, and laid-back charm. As the sun
+            sets, the space transforms into a vibrant bar with cold drinks, good music, and a chill
+            ambiance perfect for winding down or turning up. Whether you're here for a quiet moment
+            or a fun night out, Cafe Flor + Bar is your all-day escape for comfort, connection, and
+            unforgettable sips.
+          </p>
+        </div>
+        <div class="bottom-section">
+          <div class="horizontal-line"></div>
+        </div>
+      </div>
+    </div>
+    <div class="second-row">
+      <div class="left-side">
+        <img src="../../../assets/background-images/about2.png" alt="About Us" />
+        <p>
+          DELIGHTFUL <br />
+          EXPERIENCE
+        </p>
+      </div>
+      <div class="right-side">
+        <div class="top-section2">
+          <p>VISION & MISSION</p>
+          <p class="description2">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </p>
+          <p class="description2">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </p>
+          <p class="description2">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+        <div class="bottom-section2">
+          <div class="horizontal-line2"></div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,3 +80,6 @@ export default {
   name: 'AboutPage',
 };
 </script>
+<style lang="scss" scoped>
+@import './AboutPage.scss';
+</style>
